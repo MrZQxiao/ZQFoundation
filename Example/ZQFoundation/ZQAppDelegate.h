4c1aec1d3b195eff8dde771b78efeec58bd10286
liy49 <895825110@qq.com>
2023-08-16 18:21:47 +0800

    初始化

---
+++ Example/ZQFoundation/ZQAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ZQAppDelegate.h
+//  ZQFoundation
+//
+//  Created by liy49 on 08/16/2023.
+//  Copyright (c) 2023 liy49. All rights reserved.
+//
+
+@import UIKit;
+
+@interface ZQAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/ZQFoundation/ZQViewController.h
@@ -0,0 +1,13 @@
+//
+//  ZQViewController.h
+//  ZQFoundation
+//
+//  Created by liy49 on 08/16/2023.
+//  Copyright (c) 2023 liy49. All rights reserved.
+//
+
+@import UIKit;
+
+@interface ZQViewController : UIViewController
+
+@end
